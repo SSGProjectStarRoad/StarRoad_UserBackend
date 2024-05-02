9abb231966a426d3b6af85fccf0c7744056462cd
HRiver <sanscout@naver.com>
2024-05-02 12:09:48 +0900

    develop/add test log4j2

---
+++ src/test/java/com/ssg/starroad/reward/repository/RewardRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.ssg.starroad.reward.repository;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Log4j2
+public class RewardRepositoryTest {
+}

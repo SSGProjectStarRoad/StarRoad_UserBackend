b26a8a2342db6e2b5e4ad4839ed3431d6addea58
ekmbjh <ekmbjh@naver.com>
2024-05-12 22:03:41 +0900

    #30 리뷰 영수증 기능 구현

--- src/main/java/com/ssg/starroad/review/DTO/ReviewReceiptDTO.java
+++ src/main/java/com/ssg/starroad/review/DTO/ReviewReceiptDTO.java
@@ -1,5 +1,6 @@
 package com.ssg.starroad.review.DTO;
 
+import com.ssg.starroad.review.entity.ReviewReceipt;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,4 +17,13 @@ public class ReviewReceiptDTO {
     private String paymentType;
     private String approvalNumber;
     private LocalDateTime purchaseDate;
+
+    public ReviewReceipt toEntity() {
+        return ReviewReceipt.builder()
+                .shopName(this.shopName)
+                .paymentType(this.paymentType)
+                .approvalNumber(this.approvalNumber)
+                .purchaseDate(this.purchaseDate)
+                .build();
+    }
 }

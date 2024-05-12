746834744ce95b9bd054b01e665804a16d73411c
ekmbjh <ekmbjh@naver.com>
2024-05-12 23:11:27 +0900

    #37 리뷰 키워드 기능 추가

---
+++ src/main/java/com/ssg/starroad/review/DTO/ReviewKeywordDTO.java
@@ -0,0 +1,11 @@
+package com.ssg.starroad.review.DTO;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ReviewKeywordDTO {
+    private Long id;
+    private String name;
+}
--- src/main/java/com/ssg/starroad/review/service/impl/ReviewKeywordServiceImpl.java
+++ src/main/java/com/ssg/starroad/review/service/impl/ReviewKeywordServiceImpl.java
@@ -1,10 +1,39 @@
 package com.ssg.starroad.review.service.impl;
 
+import com.ssg.starroad.review.DTO.ReviewKeywordDTO;
+import com.ssg.starroad.review.entity.ReviewKeyword;
+import com.ssg.starroad.review.repository.ReviewKeywordRepository;
 import com.ssg.starroad.review.service.ReviewKeywordService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewKeywordServiceImpl implements ReviewKeywordService {
+
+    private final ReviewKeywordRepository reviewKeywordRepository;
+
+    @Override
+    public ReviewKeywordDTO createReviewKeyword(ReviewKeywordDTO reviewKeywordDTO) {
+        ReviewKeyword reviewKeyword = ReviewKeyword.builder()
+                .name(reviewKeywordDTO.getName())
+                .build();
+
+        reviewKeyword = reviewKeywordRepository.save(reviewKeyword);
+
+        return ReviewKeywordDTO.builder()
+                .id(reviewKeyword.getId())
+                .name(reviewKeyword.getName())
+                .build();
+    }
+
+    @Override
+    public Optional<ReviewKeywordDTO> findReviewKeywordById(Long id) {
+        return reviewKeywordRepository.findById(id)
+                .map(keyword -> ReviewKeywordDTO.builder()
+                        .id(keyword.getId())
+                        .name(keyword.getName())
+                        .build());
+    }
 }

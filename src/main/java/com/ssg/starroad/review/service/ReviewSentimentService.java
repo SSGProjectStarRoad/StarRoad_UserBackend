3ab232ca4f88f7ddb647c7c52108c362aa1b5835
ekmbjh <ekmbjh@naver.com>
2024-05-12 23:01:28 +0900

    #36 리뷰 센티먼트 기능 추가

--- src/main/java/com/ssg/starroad/review/service/ReviewSentimentService.java
+++ src/main/java/com/ssg/starroad/review/service/ReviewSentimentService.java
@@ -1,4 +1,19 @@
 package com.ssg.starroad.review.service;
 
+import com.ssg.starroad.review.DTO.ReviewSentimentDTO;
+
+import java.util.Optional;
+
 public interface ReviewSentimentService {
+    // 리뷰 감성 분석 데이터 생성
+    ReviewSentimentDTO createReviewSentiment(ReviewSentimentDTO reviewSentimentDTO);
+
+    // ID를 통한 리뷰 감성 분석 데이터 조회
+    Optional<ReviewSentimentDTO> findById(Long id);
+
+    // 리뷰 감성 분석 데이터 업데이트
+    ReviewSentimentDTO updateReviewSentiment(Long id, ReviewSentimentDTO reviewSentimentDTO);
+
+    // 리뷰 감성 분석 데이터 삭제
+    void deleteReviewSentiment(Long id);
 }

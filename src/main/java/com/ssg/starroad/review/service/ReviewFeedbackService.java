5be22127568c37740b03725bfdfbdcbe76bc50f3
ekmbjh <ekmbjh@naver.com>
2024-05-12 22:03:17 +0900

    #34 리뷰 질문 기능 구현

--- src/main/java/com/ssg/starroad/review/service/ReviewFeedbackService.java
+++ src/main/java/com/ssg/starroad/review/service/ReviewFeedbackService.java
@@ -1,12 +1,20 @@
 package com.ssg.starroad.review.service;
 
 import com.ssg.starroad.review.DTO.ReviewFeedbackDTO;
-import com.ssg.starroad.review.entity.ReviewFeedback;
 
 import java.util.List;
 
 public interface ReviewFeedbackService {
 
-
+    // 특정 리뷰에 대한 피드백 조회
     List<ReviewFeedbackDTO> getReviewFeedback(Long reviewId);
+
+    // 피드백 추가
+    ReviewFeedbackDTO addReviewFeedback(ReviewFeedbackDTO reviewFeedbackDTO);
+
+    // 피드백 수정
+    ReviewFeedbackDTO updateReviewFeedback(Long feedbackId, ReviewFeedbackDTO reviewFeedbackDTO);
+
+    // 피드백 삭제
+    void deleteReviewFeedback(Long feedbackId);
 }

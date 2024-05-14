8bdc8a6b72ad3e56f8708224e59642f833f177a7
HRiver <sanscout@naver.com>
2024-05-14 18:24:14 +0900

    #40 mypage: make mypage data

--- src/main/java/com/ssg/starroad/user/service/UserService.java
+++ src/main/java/com/ssg/starroad/user/service/UserService.java
@@ -1,4 +1,7 @@
 package com.ssg.starroad.user.service;
 
+import com.ssg.starroad.user.dto.MypageDTO;
+
 public interface UserService {
+    public MypageDTO getMypage(Long id);
 }
--- src/main/java/com/ssg/starroad/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/ssg/starroad/user/service/impl/UserServiceImpl.java
@@ -1,10 +1,23 @@
 package com.ssg.starroad.user.service.impl;
 
+import com.ssg.starroad.user.dto.MypageDTO;
+import com.ssg.starroad.user.repository.UserRepository;
 import com.ssg.starroad.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    private final ModelMapper modelMapper;
+
+
+    public MypageDTO getMypage(Long id) {
+        return userRepository.findById(id).map(info -> modelMapper.map(info, MypageDTO.class)).orElse(null);
+    }
+
 }

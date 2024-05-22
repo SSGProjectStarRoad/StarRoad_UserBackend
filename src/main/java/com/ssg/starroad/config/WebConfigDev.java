80b610003a664827b963cadc411a19a2f58bdc6b
HRiver <sanscout@naver.com>
2024-05-22 11:05:25 +0900

    fix cors options

--- src/main/java/com/ssg/starroad/config/WebConfigDev.java
+++ src/main/java/com/ssg/starroad/config/WebConfigDev.java
@@ -1,11 +1,13 @@
 package com.ssg.starroad.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Profile("development")
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfigDev implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")

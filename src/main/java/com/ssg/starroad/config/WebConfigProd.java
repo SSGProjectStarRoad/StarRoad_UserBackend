3372d07a8299d409445bda9243b477ed66bed919
HRiver <sanscout@naver.com>
2024-05-22 11:24:58 +0900

    fix cors options

--- src/main/java/com/ssg/starroad/config/WebConfigProd.java
+++ src/main/java/com/ssg/starroad/config/WebConfigProd.java
@@ -11,7 +11,7 @@ public class WebConfigProd implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://10.0.15.6:8080")
+                .allowedOrigins("http://10.0.15.6")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
